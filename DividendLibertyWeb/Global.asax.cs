d39d97e7df7ed338495da646b073645e37825966
Steve Yi <steveyi32@gmail.com>
2016-02-18 14:26:01 -0600

    added bootstrap example.

--- DividendLibertyWeb/Global.asax.cs
+++ DividendLibertyWeb/Global.asax.cs
@@ -14,15 +14,73 @@ namespace DividendLibertyWeb
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //"Account", // Route name
+            //"{controller}/{action}", // URL with parameters
+            //new { controller = "Account", action = "Login", id = UrlParameter.Optional } // Parameter defaults
+            //);
+
+            routes.MapRoute(
+            "Home", // Route name
+            "{controller}/{action}", // URL with parameters
+            new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class SessionExpireFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContext ctx = HttpContext.Current;
+
+            // If the browser session or authentication session has expired...
+            if (ctx.Session["UserName"] == null || !filterContext.HttpContext.Request.IsAuthenticated)
+            {
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    // For AJAX requests, we're overriding the returned JSON result with a simple string,
+                    // indicating to the calling JavaScript code that a redirect should be performed.
+                    filterContext.Result = new JsonResult { Data = "_Logon_" };
+                }
+                else
+                {
+                    // For round-trip posts, we're forcing a redirect to Home/TimeoutRedirect/, which
+                    // simply displays a temporary 5 second notification that they have timed out, and
+                    // will, in turn, redirect to the logon page.
+                    //    filterContext.Result = new RedirectToRouteResult(
+                    //        new RouteValueDictionary {
+                    //        { "Controller", "User" },
+                    //        { "Action", "Login" }
+                    //});
+                    ctx.Response.Redirect("http://www.coastalflow.com/online_part_order/User/Login");
+                }
+            }
+
+            base.OnActionExecuting(filterContext);
         }
     }
 }
---
+++ DividendLibertyWeb/Scripts/Home.js
@@ -0,0 +1,50 @@
+﻿
+
+$(document).ready(function () {
+
+});
+
+function GetPath(name) {
+    var urlpath = "";
+    var environ = window.location.host;
+    if (environ.indexOf("localhost") != -1 || environ.indexOf("cfmdev1") != -1) {
+        urlpath = "/" + name;
+    }
+    else {
+        urlpath = "/online_part_order/" + name;
+        //urlpath = name;
+    }
+    return urlpath;
+}
+
+function Login() {
+    var userModel = {
+        "UserName": $('#usrname').val(),
+        "Password": $('#psw').val(),
+        "RememberMe": false
+    }
+    $.ajax({
+        beforeSend: function () {
+            //ShowLoadingImg(true, 'dvLoading');
+        },
+        complete: function () {
+            //ShowLoadingImg(false, 'dvLoading');
+        },
+        url: "Account/test",
+        //url: "Home/test",
+        type: "POST",
+        contentType: 'application/html; charset=utf-8',
+        dataType: "json",
+        data: { s: "s"},
+        //data: { UserName: username, Password: password },
+        //data: JSON.stringify(userModel),
+        async: false,
+        cache: false,
+        success: function (data) {
+
+        },
+        error: function (data) {
+
+        }
+    });
+}
